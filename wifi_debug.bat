3ad6298a223953a2d3cf61fafb76d95c05411ae4
ishakaa <37158905+ishakaa@users.noreply.github.com>
2021-10-20 22:34:58 +0300

    Create wifi_debug.bat

---
+++ wifi_debug.bat
@@ -0,0 +1,4 @@
+:: Run this script to debug from android phone with android studio.
+cd C:\Users\hp\AppData\Local\Android\Sdk\platform-tools
+adb pair 192.168.1.46:42507
+adb connect 192.168.1.46:37087
